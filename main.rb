1e16cedfcb99c244590ee4c22b16351ada0fb16f
Jolvera2001 <jolvera6@stedwards.edu>
2025-06-05 11:35:20 -0500

    testing parameter fetching

--- main.rb
+++ main.rb
@@ -6,3 +6,19 @@ set :haml, :format => :html5
 get "/" do
   haml :index
 end
+
+get "/profile" do
+  haml :profile
+end
+
+get "/dashboard" do
+  haml :dashboard
+end
+
+get "/users/:stub" do
+  @stub = params[:stub]
+  @user_name = 'Test User'
+  @page_title = "#{@user_name}'s Profile: Stub is - #{@stub}"
+
+  haml :user
+end
