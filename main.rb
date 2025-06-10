ba6a1bdc92cf15b87a8f8ea800f960af4cebcda5
Jolvera2001 <jolvera6@stedwards.edu>
2025-06-10 17:25:17 -0500

    getting pages set up with basic sign up page and encryption

--- main.rb
+++ main.rb
@@ -9,34 +9,23 @@ require_relative 'models/test_model'
 
 set :haml, :format => :html5
 
+# Views
+
 get "/" do
   haml :index
 end
 
-get "/profile" do
+get "/signup" do
+  haml :signup
+end
+
+get "/profile/:id" do
+  id = params['id']
   haml :profile
 end
 
-get "/dashboard" do
-  haml :dashboard
-end
+# APIs
 
-get "/users/:stub" do
-  @stub = params[:stub]
-  @user_name = 'Test User'
-  @page_title = "#{@user_name}'s Profile: Stub is - #{@stub}"
+post "/signup" do
 
-  haml :user
-end
-
-# Testing Mongoid here
-
-post "/testadd" do
-  TestModel.create!(name: 'Test Man!', age: 50)
-  "TestModel Created!"
-end
-
-get "/testseeadded" do
-  content_type :json
-  TestModel.all.to_json
 end
--- models/user.rb
+++ models/user.rb
@@ -1,6 +1,15 @@
+require 'bcrypt'
 class User
   include Mongoid::Document
 
   field :email, type: String
-  field :password, type: String
+  field :password_hash, type: String
+
+  def password=(new_password)
+    self.password_hash = BCrypt::Password.create(new_password, cost: 12)
+  end
+
+  def authenticate(password)
+    BCrypt::Password.new(password_hash) == password
+  end
 end
